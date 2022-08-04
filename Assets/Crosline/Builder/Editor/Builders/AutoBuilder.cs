d46731d5cd54fc0efb366ffc3feca8b4d53ec343
Crosline <cagkancaglayanel@gmail.com>
2022-08-04 10:24:12 +0300

    Add: AutoBuilder.cs

---
+++ Assets/Crosline/Builder/Editor/Builders/AutoBuilder.cs
@@ -0,0 +1,5 @@
+﻿namespace Crosline.Builder.Editor.Builders {
+    public class AutoBuilder {
+        //TODO - Crosline: Don't forget to implement an Auto Build for CI system like Jenkins etc.
+    }
+}
