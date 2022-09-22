b6d39d35ba496b47e4ce46787eb465a0f21e7ed2
Crosline <cagkancaglayanel@gmail.com>
2022-09-22 20:33:21 +0300

    Add: New build asset

--- Assets/Crosline/Editor/BuildTools/BuildSteps/AdjustPlayerSettings.cs
+++ Assets/Crosline/Editor/BuildTools/BuildSteps/AdjustPlayerSettings.cs
@@ -5,6 +5,7 @@ namespace Crosline.BuildTools.Editor.BuildSteps {
     public class AdjustPlayerSettings : BuildStep {
         public override bool Execute() {
             PlayerSettings.bundleVersion = CommonBuilder.Instance.buildConfig.version;
+            PlayerSettings.applicationIdentifier = CommonBuilder.Instance.buildConfig.bundle;
             Debug.Log($"builder instance {CommonBuilder.Instance != null}");
             Debug.Log($"buildConfig instance {CommonBuilder.Instance.buildConfig != null}");
             PlayerSettings.SetScriptingBackend(CommonBuilder.Instance.BuildPlatform.ToBuildTargetGroup(), CommonBuilder.Instance.buildConfig.backend);
