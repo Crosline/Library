6611e71d80df81935c120a8ce6e0ce1e9ba3e0d4
Crosline <cagkancaglayanel@gmail.com>
2022-09-22 21:01:57 +0300

    Re: Direct casting

--- Assets/Crosline/Editor/BuildTools/BuildSteps/AdjustPlayerSettings.cs
+++ Assets/Crosline/Editor/BuildTools/BuildSteps/AdjustPlayerSettings.cs
@@ -6,13 +6,12 @@ namespace Crosline.BuildTools.Editor.BuildSteps {
         public override bool Execute() {
             PlayerSettings.bundleVersion = CommonBuilder.Instance.buildConfig.version;
             PlayerSettings.applicationIdentifier = CommonBuilder.Instance.buildConfig.bundle;
-            Debug.Log($"builder instance {CommonBuilder.Instance != null}");
-            Debug.Log($"buildConfig instance {CommonBuilder.Instance.buildConfig != null}");
+            
             PlayerSettings.SetScriptingBackend(CommonBuilder.Instance.BuildPlatform.ToBuildTargetGroup(), CommonBuilder.Instance.buildConfig.backend);
 
             switch (CommonBuilder.Instance.BuildPlatform) {
                 case BuildOptions.BuildPlatform.Android:
-                    PlayerSettings.Android.targetArchitectures = (CommonBuilder.Instance.buildConfig as AndroidBuildConfigAsset).architecture;
+                    PlayerSettings.Android.targetArchitectures = ((AndroidBuildConfigAsset) CommonBuilder.Instance.buildConfig).architecture;
 
                     break;
 
