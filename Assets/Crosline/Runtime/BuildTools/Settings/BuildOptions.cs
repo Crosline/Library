bb733c38113df880a7b34748d0292e62a5b82901
Crosline <cagkancaglayanel@gmail.com>
2022-09-28 17:31:04 +0300

    Fix: BuildOptions

--- Assets/Crosline/Runtime/BuildTools/Settings/BuildOptions.cs
+++ Assets/Crosline/Runtime/BuildTools/Settings/BuildOptions.cs
@@ -2,30 +2,30 @@
     public class BuildOptions {
         
         public enum BuildPlatform {
-            Windows = 0,
-            Linux = 1,
-            MacOS = 2,
-            Android = 3,
-            IOS = 4,
+            Windows = 1 << 0,
+            Linux = 1 << 1,
+            MacOS = 1 << 2,
+            Android = 1 << 3,
+            IOS = 1 << 4,
             Mobile = Android | IOS,
             Standalone = Windows | Linux | MacOS,
             Generic = Standalone | Mobile
         }
         
         public enum ApiCompability {
-            NetStandard21 = 0,
-            NetFramework = 1
+            NetStandard21 = 1 << 0,
+            NetFramework = 1 << 1
         }
         
         public enum Compression {
-            None = 0,
-            CompressWithLz4 = 1,
-            CompressWithLz4HC = 2
+            None = 1 << 0,
+            CompressWithLz4 = 1 << 1,
+            CompressWithLz4HC = 1 << 2
         }
 
         public enum BuildMode {
-            DEBUG = 0,
-            RELEASE = 1
+            DEBUG = 1 << 0,
+            RELEASE = 1 << 1
         }
     }
 }
