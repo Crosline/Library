585d2d73bdd54cef50fcd2b4fd8e0c566a5c4558
Crosline <cagkancaglayanel@gmail.com>
2022-10-26 22:56:55 +0300

    Update: Editor ifs and attributes

--- Assets/Crosline/Runtime/UnityTools/Attributes/MethodAttribute.cs
+++ Assets/Crosline/Runtime/UnityTools/Attributes/MethodAttribute.cs
@@ -3,14 +3,13 @@
 namespace Crosline.UnityTools.Attributes {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class MethodAttribute : Attribute {
-
         public readonly object[] Parameters;
 
-        public MethodAttribute() {
+        protected MethodAttribute() {
             Parameters = null;
         }
 
-        public MethodAttribute(params object[] parameters) {
+        protected MethodAttribute(params object[] parameters) {
             Parameters = parameters;
         }
     }
