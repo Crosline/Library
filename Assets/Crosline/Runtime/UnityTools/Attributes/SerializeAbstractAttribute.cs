e19e85b6163a5bde0fd78522db25632d5545774c
Crosline <cagkancaglayanel@gmail.com>
2023-02-10 15:29:05 +0300

    TODO: my own odin inspector cuz its awesome

--- Assets/Crosline/Runtime/UnityTools/Attributes/SerializeAbstractAttribute.cs
+++ Assets/Crosline/Runtime/UnityTools/Attributes/SerializeAbstractAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 namespace Crosline.UnityTools.Attributes {
+    [System.Obsolete("Use ODIN inspector and SerializeReference instead.")]
     public class SerializeAbstractAttribute : PropertyAttribute {
     }
 }
