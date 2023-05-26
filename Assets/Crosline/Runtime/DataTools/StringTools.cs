553c07f020235c837ea75d1e3dc87b17a92f7b27
Crosline <cagkancaglayanel@gmail.com>
2023-05-26 17:54:29 +0300

    Add: Base for runtime DataTools

---
+++ Assets/Crosline/Runtime/DataTools/StringTools.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Crosline.DataTools {
+    public static class StringTools {
+        public static bool IsAscii(this char c) {
+            return c <= sbyte.MaxValue;
+        }
+        
+        public static bool IsAscii(this string str) {
+            return str.All(c => c.IsAscii());
+        }
+        
+        public static int CountSubstring(this string text, string subString)
+        {
+            return (text.Length - text.Replace(subString, "").Length) / subString.Length;
+        }
+    }
+}
