bf5628e16658bffb8f9b84ed5d90a57b132ea6f0
Crosline <cagkancaglayanel@gmail.com>
2023-05-26 17:54:44 +0300

    Add: Assert options for CroslineDebug logs

---
+++ Assets/Crosline/Runtime/DebugTools/Log/Assert.cs
@@ -0,0 +1,29 @@
+﻿#if CROSLINE_DEBUG || UNITY_EDITOR
+#define DEBUG_EDITOR
+#endif
+
+
+#if DEBUG_EDITOR
+using UnityEngine;
+#endif
+
+namespace Crosline.DebugTools {
+    public static partial class CroslineDebug {
+        public static void Assert(bool condition, string log, string prefix = "") {
+#if DEBUG_EDITOR
+            Debug.Assert(condition, $"[{GetPrefix(prefix)}] {log}");
+#endif
+        }
+
+        public static void AssertException(bool condition, string log, System.Exception e, string prefix = "") {
+#if DEBUG_EDITOR
+            if (condition)
+                return;
+
+            Debug.Assert(false, $"[{GetPrefix(prefix)}] {log}");
+
+            throw e;
+#endif
+        }
+    }
+}
