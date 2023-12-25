acb66550482100ee33c66851e05e19eeb9c099ff
Crosline <cagkancaglayanel@gmail.com>
2023-12-26 01:36:58 +0300

    ..

--- Assets/Crosline/Runtime/Game/Lifecycle/GameSystemBase.cs
+++ Assets/Crosline/Runtime/Game/Lifecycle/GameSystemBase.cs
@@ -6,11 +6,12 @@ namespace Crosline.Game.Lifecycle {
     [Serializable]
     public abstract class GameSystemBase {
 
-        protected GameSystemBase() { }
+        protected GameSystemBase() {
+        }
 
         [field: SerializeField]
         public bool IsDevelopmentOnly { get; private set; }
-        
+
         public abstract Task Initialize();
         public abstract void Dispose();
     }
