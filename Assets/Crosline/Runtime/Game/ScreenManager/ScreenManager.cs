162e21f79c33a223d6f0ab39011590c4e6bb7c34
Crosline <cagkancaglayanel@gmail.com>
2024-01-04 01:50:43 +0300

    ..

--- Assets/Crosline/Runtime/Game/ScreenManager/ScreenManager.cs
+++ Assets/Crosline/Runtime/Game/ScreenManager/ScreenManager.cs
@@ -4,8 +4,8 @@ using Crosline.Game.Lifecycle;
 namespace Crosline.Game.ScreenManager {
     public class ScreenManager : GameSystemBase, IUpdateListener {
 
-        public override Task Initialize() {
-            throw new System.NotImplementedException();
+        public override async Task Initialize() {
+            await Task.Delay(5_000);
         }
 
         public override void Dispose() {
