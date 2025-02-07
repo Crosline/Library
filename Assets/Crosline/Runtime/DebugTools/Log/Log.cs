0bc5f51330969fcc509979e0f09fa8ee5db5a72b
Crosline <cagkancaglayanel@gmail.com>
2025-02-07 13:37:16 +0300

    Update logcs

--- Assets/Crosline/Runtime/DebugTools/Log/Log.cs
+++ Assets/Crosline/Runtime/DebugTools/Log/Log.cs
@@ -2,6 +2,9 @@
 #define DEBUG_EDITOR
 #endif
 
+using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 #if DEBUG_EDITOR
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,15 +13,16 @@ using UnityEngine;
 namespace Crosline.DebugTools {
     public static partial class CroslineDebug {
 #if DEBUG_EDITOR
+        [DebuggerHidden]
         private static string DefaultPrefix {
             get {
-                var stackTraceOutput = StackTraceUtility.ExtractStackTrace().Split('\n')[4];
-                int spaceIndex = stackTraceOutput.IndexOf(' ') - 1;
+                var stackTraceOutput = StackTraceUtility.ExtractStackTrace().Split('\n')[5];
+                var spaceIndex = stackTraceOutput.IndexOf(' ') - 1;
 
-                List<char> prefixList = new List<char>();
+                var prefixList = new List<char>();
 
-                for (int i = spaceIndex; i >= 0; i--) {
-                    char c = stackTraceOutput[i];
+                for (var i = spaceIndex; i >= 0; i--) {
+                    var c = stackTraceOutput[i];
 
                     if (c.Equals('.'))
                         break;
@@ -27,39 +31,45 @@ namespace Crosline.DebugTools {
                 }
 
                 prefixList.Reverse();
-                char[] prefixArray = prefixList.ToArray();
+                var prefixArray = prefixList.ToArray();
 
                 return new string(prefixArray);
             }
         }
 #endif
 
+        [DebuggerHidden]
         public static void Log(string log, string prefix = "") {
 #if DEBUG_EDITOR
-            Debug.Log(BuildLog(log, prefix));
+            Console.WriteLine(BuildLog(log, prefix));
 #endif
         }
 
+        [DebuggerHidden]
         public static void LogWarning(string log, string prefix = "") {
 #if DEBUG_EDITOR
             Debug.LogWarning(BuildLog(log, prefix));
 #endif
         }
 
+        [DebuggerHidden]
         public static void LogError(string log, string prefix = "") {
 #if DEBUG_EDITOR
             Debug.LogError(BuildLog(log, prefix));
 #endif
         }
+
 #if DEBUG_EDITOR
+        [DebuggerHidden]
         private static string GetPrefix(string prefix) {
             return string.IsNullOrEmpty(prefix) ? DefaultPrefix : prefix;
         }
-        
+
+        [DebuggerHidden]
         private static string GetStylizedPrefix(string prefix) {
             return $"<size=14><color=#808080>{GetPrefix(prefix)}</size></color>";
         }
-        
+
         private static string BuildLog(string log, string prefix) {
             return $"[{GetPrefix(prefix)}] {log}";
         }
