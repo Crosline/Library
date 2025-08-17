5ae018d1c90b556e6768dfe1358e43eb55eb75c4
Crosline <cagkancaglayanel@gmail.com>
2025-08-17 17:51:32 +0300

    0.1.22

--- Assets/Crosline/Runtime/Subsystems/GameSubsystem.cs
+++ Assets/Crosline/Runtime/Subsystems/GameSubsystem.cs
@@ -4,7 +4,23 @@ using Serializables;
 namespace Subsystems
 {
     public abstract class GameSubsystem<T> : GameSubsystem where T : GameSubsystem<T>
-    { }
+    {
+
+        public override void Initialize() {
+        }
+        public override void Shutdown() {
+        }
+        public override void OnAwake() {
+        }
+        public override void OnDestroy() {
+        }
+        public override void OnApplicationQuit() {
+        }
+        public override void OnApplicationFocus(bool hasFocus) {
+        }
+        public override void OnApplicationPause(bool hasPaused) {
+        }
+    }
 
     [Serializable]
     public abstract class GameSubsystem
